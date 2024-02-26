20907f2d9fccae28f0148f5c17b9fc7fb666f055
juye-ops <kjye.ops@gmail.com>
2024-02-26 18:39:21 +0900

    Feat: Refactor const data

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <style.Footer>
         <br></br>
         <style.TextP1>본 서비스는 사설로 운영됩니다.</style.TextP1>
-        <style.TextP1>상담 연결: <style.Contact href="https://open.kakao.com/o/suUsJwYf">https://open.kakao.com/o/suUsJwYf</style.Contact></style.TextP1>
+        <style.TextP1>상담 문의: <style.Contact href="https://open.kakao.com/o/suUsJwYf">https://open.kakao.com/o/suUsJwYf</style.Contact></style.TextP1>
         <hr></hr>
         <style.TextP2>Clouder</style.TextP2>
       </style.Footer>
